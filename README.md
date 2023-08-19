6d4faa300c4c39d543ac754f6912559cca084c7f
nadhir afsa <m.nadhir.afsa@gmail.com>
2023-08-19 12:57:57 +0100

    adding comment & modifing the readme file

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# Salesforce DX Project: Next Steps
+# Salesforce  Project: 
 
-Now that you’ve created a Salesforce DX project, what’s next? Here are some documentation resources to get you started.
+This Project is for u:fynd Salesforce assignment.
 
-## How Do You Plan to Deploy Your Changes?
+## How Do You can view The desired result ?
 
-Do you want to deploy a set of changes, or create a self-contained application? Choose a [development model](https://developer.salesforce.com/tools/vscode/en/user-guide/development-models).
+LOOK INTO THE PDF file for more information
 
-## Configure Your Salesforce DX Project
-
-The `sfdx-project.json` file contains useful configuration information for your project. See [Salesforce DX Project Configuration](https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_ws_config.htm) in the _Salesforce DX Developer Guide_ for details about this file.
 
 ## Read All About It
 
--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,7 +1,7 @@
 import { LightningElement,wire } from 'lwc';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
 
-
+// Define columns for the lightning-datatable
 const COLUMNS = [
     { label: 'First Name', fieldName: 'FirstName', type: 'text' },
     { label: 'Last Name', fieldName: 'LastName', type: 'text' },
@@ -10,15 +10,18 @@ const COLUMNS = [
 ];
 
 export default class ContactList extends LightningElement {
-
+    // Initialize columns using the defined COLUMNS constant
     columns = COLUMNS;
+    // Initialize contacts as an empty array
     contacts = [];
-
+    // Wire the getContacts() method to automatically fetch data
     @wire(getContacts)
     wiredContacts({ error, data }) {
+        // If data is available, update the contacts array
         if (data) {
             this.contacts = data;
         } else if (error) {
+        // If there's an error, log it to the console
             console.error(error);
         }
     }
